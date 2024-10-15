8372c8aceea7208189b8934bd627d7fdb3080daa
mxte-b <13c-blank@ipari.vein.hu>
2024-10-15 17:43:57 +0200

    Kijelentkezés működése megfelelő

--- src/index.php
+++ src/index.php
@@ -4,23 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Főoldal - TESZT</title>
+    <?php 
+        session_start(); 
+        $isLoggedIn = false;
+        var_dump($_SESSION);
+        if (isset($_SESSION['user_name'])) { // Ha be van jelentkezve a felhasználó
+            $sessionId = session_id();
+            $isLoggedIn = true;
+        }
+    ?>
 </head>
 <body>
     <header><h1>Főoldal</h1></header>
-    <p><a href="./login">Bejelentkezés</a></p>
+    <p>
+        <?php 
+            if ($isLoggedIn) {
+                echo "<a href='./logout'>Kijelentkezés</a>";
+            }
+            else {
+                echo "<a href='./login'>Bejelentkezés</a>";
+            }
+        ?>
+    </p>
     <p><a href="./register">Regisztráció</a></p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa aperiam excepturi pariatur, sapiente dolorem, quis iure ratione veritatis autem fugit eius tempore. Minus hic suscipit omnis explicabo illo nulla.</p>
 </body>
 </html>
 <?php
-
-    session_start();
-
-    if (isset($_SESSION['user_name'])) {
+    if ($isLoggedIn) {
         echo "Welcome, {$_SESSION['user_name']}! You have {$_SESSION['role']} role.";
-    }
-    else {
-        echo "You are not logged in.";
+        echo "Your sessionID is $sessionId";
     }
 
+    if (isset($_SESSION['successful_logout']) && $_SESSION['successful_logout']) {
+        echo "<p style='color: green;'>You have been successfully logged out.</p>";
+        unset($_SESSION['successful_logout']);
+    }
 ?>
