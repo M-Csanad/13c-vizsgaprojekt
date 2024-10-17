7f2b5040270bbd62a1157bdb1d537c5ab38db6c8
mxte-b <13c-blank@ipari.vein.hu>
2024-10-17 11:40:09 +0200

    Dizájn elkezdése

--- src/login.php
+++ src/login.php
@@ -4,20 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Egyszerű bejelentkezés</title>
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    <p><a href="./">Vissza a főoldalra</a></p>
     <form action="" method="post">
-        <label for="username">Felhasználónév: </label>
-        <input type="text" name="username" id="username" autocomplete='username'>
-        <br>
-        <label for="passwd">Jelszó: </label>
-        <input type="password" name="passwd" id="passwd" autocomplete='current-password'>
-        <br>
-        <input type="checkbox" name="rememberMe" id="rememberMe">
-        <label for="rememberMe">Maradjak bejelentkezve</label>
-        <br>
-        <input type="submit" value="Log in" name="login">
+        <div class="form-header">
+            <p>
+                <a href="./" class="form-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                    </svg>
+                    Vissza a főoldalra
+                </a>
+            </p>
+            <h1>Bejelentkezés</h1>
+        </div>
+        <div class="input-wrapper">
+            <div class="input-group">
+                <label for="username">Felhasználónév: </label>
+                <input type="text" name="username" id="username" autocomplete='username' required>
+            </div>
+            <div class="input-group">
+                <label for="passwd">Jelszó: </label>
+                <input type="password" name="passwd" id="passwd" autocomplete='current-password' required>
+            </div>
+            <div class="input-group inline">
+                <input type="checkbox" name="rememberMe" id="rememberMe">
+                <label for="rememberMe">Maradjak bejelentkezve</label>
+            </div>
+        </div>
+        <input type="submit" value="Bejelentkezés" name="login" class="action-button">
     </form>
 </body>
 </html>
