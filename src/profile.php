d1d5ad33eeb1c9e44cea317afbce5b24543e57ee
mxte-b <13c-blank@ipari.vein.hu>
2024-11-29 12:28:48 +0100

    Órai munka 2

--- src/profile.php
+++ src/profile.php
@@ -82,7 +82,7 @@
                 <div class="sidebar-bottom">
                     <?php 
                         if ($user["role"] === "Administrator") {
-                            echo "<a class='dashboard' href='./dashboard'>
+                            echo "<a class='dashboard' href='./dashboard' draggable='false'>
                                 <div class='text'>Vezérlőpult</div>
                                 <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-shield-shaded' viewBox='0 0 16 16'>
                                     <path fill-rule='evenodd' d='M8 14.933a1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56'/>
@@ -90,7 +90,7 @@
                             </a>";
                         }
                     ?>
-                    <a class="logout" href="./logout">
+                    <a class="logout" href="./logout" draggable="false">
                         <div class="text">Kijelentkezés</div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
@@ -102,7 +102,10 @@
         </div>
         <div class="content-wrapper dynamic-border"  style="--mouse-x: 50%; --mouse-y: 50%; --radius: 0px; --color:  rgb(43, 43, 43)">
             <div class="content">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim mollitia incidunt harum vero ullam illo laboriosam magni, iure iste repellat laudantium, eligendi vitae blanditiis dicta molestias provident nisi voluptates eveniet?
+                <div class="content-page active"></div>
+                <div class="content-page"></div>
+                <div class="content-page"></div>
+                <div class="content-page"></div>
             </div>
         </div>
     </div>
