020416e17fdd55be5edf44a40ce549f87a2b1159
M-Csanad <ipari.b00li@gmail.com>
2024-12-16 21:49:30 +0100

    category oldal séma létrehozva

---
+++ domains/domainname/public_html/fb-content/category.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet"
+    href="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/assets/css/box.css" />
+  <link rel="stylesheet"
+    href="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/assets/css/font.css" />
+  <link rel="stylesheet"
+    href="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/fb-categories/css/category-cards.css" />
+  <title>Category Cards</title>
+</head>
+
+<body>
+  <main>
+    <section class="Card-subcontainer_main">
+      <h1 class="section_headerProductTitle">Discover what we offer</h1>
+      <div class="flex-block" id="CardSection__subcategory">
+        <div class="col-3_inner CardContainer horizontalCard">
+          <div class="col-4_inner Card_imageWrapper Card_themeImage">
+            <img
+              src="https://images.unsplash.com/photo-1598121443852-ff347aff7520?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Sample Image 1" />
+          </div>
+          <div class="col-8_inner Card_body">
+            <div class="Card_header">
+              <div class="Card_headerProductCounter">13 products</div>
+              <div class="Card_headerProductTitle">
+                Carry Your World With Ease
+              </div>
+            </div>
+          </div>
+          <div class="card-pattern"></div>
+        </div>
+
+        <div class="col-3_inner CardContainer horizontalCard">
+          <div class="col-4_inner Card_imageWrapper Card_themeImage">
+            <img
+              src="https://images.unsplash.com/photo-1552571219-d6e38a3f4849?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Sample Image 2" />
+          </div>
+          <div class="col-8_inner Card_body">
+            <div class="Card_header">
+              <div class="Card_headerProductCounter">43 products</div>
+              <div class="Card_headerProductTitle">Discover your scent</div>
+            </div>
+          </div>
+          <div class="card-pattern"></div>
+        </div>
+      </div>
+    </section>
+    <section class="Card-container_main">
+      <h1 class="section_headerProductTitle">
+        See also the other categories
+      </h1>
+      <div class="flex-block" id="CardSection__category">
+        <div class="CardContainer verticalCard">
+          <div class="Card_header">
+            <div class="Card_headerProductCounter">50 products</div>
+            <div class="Card_headerProductTitle">Explore the world</div>
+          </div>
+          <div class="Card_body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              venenatis nunc sed risus tempor ullamcorper.
+            </p>
+          </div>
+          <div class="Card_footer">
+            <div class="Card_footerLeft">
+              <div class="Card_imageWrapper Card_logoImage">
+                <img
+                  src="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/assets/media/images/logos/around-the-globe.png"
+                  alt="Travel Logo" />
+              </div>
+            </div>
+            <div class="Card_footerRight">
+              <div class="Card_imageWrapper Card_themeImage">
+                <img
+                  src="https://images.unsplash.com/photo-1491147334573-44cbb4602074?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Travel Image" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="CardContainer verticalCard">
+          <div class="Card_header">
+            <div class="Card_headerProductCounter">30 products</div>
+            <div class="Card_headerProductTitle">Discover your scent</div>
+          </div>
+          <div class="Card_body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              venenatis nunc sed risus tempor ullamcorper.
+            </p>
+          </div>
+          <div class="Card_footer">
+            <div class="Card_footerLeft">
+              <div class="Card_imageWrapper Card_logoImage">
+                <img
+                  src="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/assets/media/images/logos/parfume.png"
+                  alt="Perfume Logo" />
+              </div>
+            </div>
+            <div class="Card_footerRight">
+              <div class="Card_imageWrapper Card_themeImage">
+                <img
+                  src="https://images.unsplash.com/photo-1598121443852-ff347aff7520?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Perfume Image" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="CardContainer verticalCard">
+          <div class="Card_header">
+            <div class="Card_headerProductCounter">193 products</div>
+            <div class="Card_headerProductTitle">Safety adventure</div>
+          </div>
+          <div class="Card_body">
+            <p>
+              Explore our top-quality camping and mountaineering gear,
+              ensuring you're equipped for every adventure.
+            </p>
+          </div>
+          <div class="Card_footer">
+            <div class="Card_footerLeft">
+              <div class="Card_imageWrapper Card_logoImage">
+                <img
+                  src="http://localhost:8080/13c-vizsgaprojekt/domains/domainname/public_html/fb-content/assets/media/images/logos/campsite.png"
+                  alt="Accessories Logo" />
+              </div>
+            </div>
+            <div class="Card_footerRight">
+              <div class="Card_imageWrapper Card_themeImage">
+                <img
+                  src="https://images.unsplash.com/photo-1552571219-d6e38a3f4849?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3jA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Trl Image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</body>
+
+</html>
---
+++ domains/domainname/public_html/index.php
@@ -0,0 +1,6 @@
+<?php
+#include_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+
+#include __DIR__ . '/fb-content/home.php';
+?>
+
