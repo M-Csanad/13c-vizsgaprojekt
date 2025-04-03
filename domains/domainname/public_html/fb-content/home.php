80a16f3832acd73e0939a7aee888f56dc1386bed
Milkovics Csanád <13c-milkovics@IPARI.SULINET.HU>
2025-04-03 13:29:27 +0200

    title átírása

--- domains/domainname/public_html/fb-content/home.php
+++ domains/domainname/public_html/fb-content/home.php
@@ -9,7 +9,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/user_config.php';
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Home - Florens Botanica</title>
+  <title>Florens Botanica</title>
 
   <style>
     body {
