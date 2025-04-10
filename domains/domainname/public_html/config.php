acce94856b94a6bb9f06566058f9f00c082a7f12
M-Csanad <ipari.b00li@gmail.com>
2025-04-11 00:50:32 +0200

    JS DevTools Shield

--- domains/domainname/public_html/config.php
+++ domains/domainname/public_html/config.php
@@ -7,6 +7,45 @@ if (!defined("ROOT_URL")) {
     define('ROOT_URL', 'http://localhost');
 }
 
+// Kimeneti pufferelés indítása a végrehajtás elején
+ob_start();
+
+// Funkció a JS védelmi fájl beillesztésére
+function includeJsProtection($buffer) {
+    if (preg_match('/<html/i', $buffer)) {
+        // Szkript beillesztése a záró </head> tag előtt, ha létezik
+        $pattern = '/<\/head>/i';
+        $replacement = '<script disable-devtool-auto src="/JS_64697361626C652D646576746F6F6C.js"></script></head>';
+
+        if (preg_match($pattern, $buffer)) {
+            $buffer = preg_replace($pattern, $replacement, $buffer, 1);
+        } else {
+            // Fallback
+            $pattern = '/<body[^>]*>/i';
+            $replacement = '$0<script disable-devtool-auto src="/JS_64697361626C652D646576746F6F6C.js"></script>';
+
+            if (preg_match($pattern, $buffer)) {
+                $buffer = preg_replace($pattern, $replacement, $buffer, 1);
+            } else {
+                // Utolsó megoldás: a pufferbe való beillesztés
+                $buffer = '<script disable-devtool-auto src="/JS_64697361626C652D646576746F6F6C.js"></script>' . $buffer;
+            }
+        }
+    }
+
+    return $buffer;
+}
+
+// Regisztrálja a függvényt, amely a parancsfájl leállításakor végrehajtódik, és feldolgozza a puffert.
+register_shutdown_function(function() {
+    // Puffer tartalmának kiolvasása és törlése
+    $buffer = ob_get_clean();
+    // Szkript injektálást
+    if ($buffer !== false) {
+        echo includeJsProtection($buffer);
+    }
+});
+
 include BASE_PATH  . '/fb-functions/maintenance/maintenanceVerify.php';
 
 if (isMaintenanceMode()) {
