f7df591744472983e7fa487a546c7118d2f1c68c
mxte-b <13c-blank@ipari.vein.hu>
2025-04-11 18:52:50 +0200

    SVG javítva

--- domains/domainname/public_html/fb-content/product.php
+++ domains/domainname/public_html/fb-content/product.php
@@ -668,7 +668,7 @@
         </div>
         <div class="navigator-button navigator-right">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
           </svg>
         </div>
       </div>
